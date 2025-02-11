767dc225a709e6090cd14183de76f8f0009b67ed
Bhavya Sonigra <sonigrabhavya28@gmail.com>
2025-02-11 12:45:11 +0530

    Project created

---
+++ src/main/java/org/example/cloth_inventory_backend/ClothInventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloth_inventory_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothInventoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothInventoryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cloth_inventory_backend/ClothInventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloth_inventory_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothInventoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
